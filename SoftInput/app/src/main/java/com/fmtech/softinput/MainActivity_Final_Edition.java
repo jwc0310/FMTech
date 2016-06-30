6588171746334baba42bf0a80e799200ef83e7d3
Drew.Chiang <jiangchuna@126.com>
2016-06-30 14:05:29 +0800

    It works good now.

--- SoftInput/app/src/main/java/com/fmtech/softinput/MainActivity_Final_Edition.java
+++ SoftInput/app/src/main/java/com/fmtech/softinput/MainActivity_Final_Edition.java
@@ -3,9 +3,9 @@ package com.fmtech.softinput;
 import android.annotation.SuppressLint;
 import android.graphics.Rect;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
@@ -13,7 +13,7 @@ import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
-public class MainActivity extends AppCompatActivity implements FMLinearLayout.OnSizeChangedListener {
+public class MainActivity_Final_Edition extends AppCompatActivity implements FMLinearLayout.OnSizeChangedListener {
 
     private FMLinearLayout mContainer;
     private ScrollView mScrollView;
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements FMLinearLayout.On
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_signup_step2);
-//        AndroidBug5497Workaround.assistActivity(this);
         mContainer = (FMLinearLayout) findViewById(R.id.container);
         mScrollView = (ScrollView) findViewById(R.id.scrollview);
         mTitleBar = (FrameLayout) findViewById(R.id.singup_navigator_step2);
@@ -44,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements FMLinearLayout.On
         mOnGlobalLayoutChangeListener = new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
+
                 int containerVisibleFrameHeight = getContainerVisibleHeight();
 
                 //getRootView ---Finds the topmost view in the current view hierarchy
@@ -56,42 +56,19 @@ public class MainActivity extends AppCompatActivity implements FMLinearLayout.On
                 if (mPreContainerVisibleHeight != containerVisibleFrameHeight) {
                     final int heightDiff = containerHeight - containerVisibleFrameHeight;
 
-                    System.out.println("-------mContainer Height: " + mContainer.getHeight());
-                    System.out.println("-------mTitleBar Height: " + mTitleBar.getHeight());
-                    System.out.println("-------heightDiff: " + heightDiff);
-                    System.out.println("-------mScrollViewHeight: " + mScrollView.getHeight());
-                    System.out.println("-------containerVisibleHeight: " + getContainerVisibleHeight());
-                    System.out.println("--------------------");
-//                    mContainer.getViewTreeObserver().removeOnGlobalLayoutListener(mOnGlobalLayoutChangeListener);
-//                    mContainer.requestLayout();
                     if (heightDiff > screenHeight / 3) {
                         mScrollView.getLayoutParams().height = mContainer.getHeight() - titleBarHeight - heightDiff;
                         mScrollView.requestLayout();//very important
-//                        mPaddingView.getLayoutParams().height= heightDiff;
-//                        mPaddingView.requestLayout();//very important
                     } else {
                         mScrollView.getLayoutParams().height = mContainer.getHeight() - titleBarHeight;
                         mScrollView.requestLayout();//very important
-//                        mPaddingView.getLayoutParams().height = 0;
-//                        mPaddingView.requestLayout();//very important
                     }
-//                    mContainer.getViewTreeObserver().addOnGlobalLayoutListener(mOnGlobalLayoutChangeListener);
 
                     mPreContainerVisibleHeight = containerVisibleFrameHeight;
-//                    mContainer.getRootView().requestLayout();
+
                     printInfo2();
 
-//                    mHandler.post(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            int[] location1 = new int[2];
-//                            mContainer.getLocationOnScreen(location1);
-//                            System.out.println("-------mHandler--->mContainer screenX:"+location1[0] +" screenY:"+location1[1]);
-//                        }
-//                    });
                 }
-
-//                printInfo2();
             }
         };
 
@@ -101,38 +78,16 @@ public class MainActivity extends AppCompatActivity implements FMLinearLayout.On
             public boolean onPreDraw() {
                 int[] location1 = new int[2];
                 mContainer.getLocationOnScreen(location1);
-                System.out.println("-------onPreDraw---mContainer screenX:" + location1[0] + " screenY:" + location1[1]);
                 if (location1[1] < 0) {
-//                    if(mPreScreenY != location1[1]){
-//                        mPreScreenY = Math.min(mPreScreenY, location1[1]);
-//                    }else{
-//                        mScrollView.getLayoutParams().height = mScrollView.getLayoutParams().height + Math.abs(mPreScreenY);
-//                        mScrollView.requestLayout();
-//                    }
-//                    mScrollView.getLayoutParams().height = mScrollView.getLayoutParams().height + Math.abs(location1[1]);
-//                    mScrollView.requestLayout();
-
-                    /*if(mPreScreenY != location1[1]){
-                        mPreScreenY = Math.min(mPreScreenY, location1[1]);
-                    }else {
-                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) mTitleBar.getLayoutParams();
-                        layoutParams.setMargins(0, Math.abs(location1[1]), 0, 0);
-                        mTitleBar.setLayoutParams(layoutParams);
-//                        mContainer.getViewTreeObserver().removeOnPreDrawListener(mOnPreDrawListener);
-                        mPreScreenY = 0;
-                    }*/
-
                     RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) mTitleBar.getLayoutParams();
                     layoutParams.setMargins(0, Math.abs(location1[1]), 0, 0);
                     mTitleBar.setLayoutParams(layoutParams);
-//                        mContainer.getViewTreeObserver().removeOnPreDrawListener(mOnPreDrawListener);
                 } else {
                     RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) mTitleBar.getLayoutParams();
                     layoutParams.setMargins(0, 0, 0, 0);
                     mTitleBar.setLayoutParams(layoutParams);
                 }
 
-//                mContainer.getViewTreeObserver().removeOnPreDrawListener(mOnPreDrawListener);
                 return true;
             }
         };
@@ -151,17 +106,12 @@ public class MainActivity extends AppCompatActivity implements FMLinearLayout.On
         System.out.println("-------mScrollView screenX:" + location1[0] + " screenY:" + location1[1]);
         mScrollView.getLocationInWindow(location1);
         System.out.println("-------mScrollView windowX:" + location1[0] + " windowY:" + location1[1]);
-//                System.out.println("-------mContainer getTranslationY:"+mContainer.getTranslationY());
-//                System.out.println("-------mScrollView getTranslationY:"+mScrollView.getTranslationY());
-//                System.out.println("-------mContainer scrollY:"+mContainer.getScrollY());
-//                System.out.println("-------mScrollView scrollY:"+mScrollView.getScrollY());
 
         System.out.println("-------mContainer Height2: " + mContainer.getHeight());
         System.out.println("-------mTitleBar Height2: " + mTitleBar.getHeight());
         System.out.println("-------mScrollViewHeight2: " + mScrollView.getHeight());
         System.out.println("-------containerVisibleHeight2: " + getContainerVisibleHeight());
         System.out.println("-------scrollViewVisibleHeight2: " + getScrollViewVisibleHeight());
-//                System.out.println("-------mScrollView Content Height2: " + mScrollView.getChildAt(0).getHeight());
         System.out.println("--------------------------------------------------");
     }
 
@@ -179,7 +129,7 @@ public class MainActivity extends AppCompatActivity implements FMLinearLayout.On
 
     @Override
     public void sizeChanged(int widthDiff, int heightDiff) {
-        System.out.println("-------sizeChanged---heightDiff:" + heightDiff);
+
     }
 
     public interface OnSoftKeyboardStateChangedListener {
