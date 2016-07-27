03be62625e1ad460b2e598f5f34904bcab6a0cd9
Drew.Chiang <drew_jiang@mtelnet.com>
2016-07-27 20:13:00 +0800

    JNI

---
+++ JNI/TestJni/obj/local/arm64-v8a/objs/TestJniDrew/com_example_testcpp_JniClient.o.d
@@ -0,0 +1,5 @@
+obj/local/arm64-v8a/objs/TestJniDrew/com_example_testcpp_JniClient.o: \
+ jni/com_example_testcpp_JniClient.cpp \
+ jni/com_example_testcpp_JniClient.h
+
+jni/com_example_testcpp_JniClient.h:
